a130c74187e0f68c46ee52457cfe4f65738523b0
carme <00029613@uca.edu.sv>
2018-04-14 19:03:56 -0600

    Creacion de fragments

--- app/src/main/java/com/example/carme/carmenfragments/FragmentList.java
+++ app/src/main/java/com/example/carme/carmenfragments/FragmentList.java
@@ -1,55 +1,17 @@
 package com.example.carme.carmenfragments;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.app.ListFragment;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
-public class FragmentList extends ListFragment implements AdapterView.OnItemClickListener{
+public class FragmentList extends ListFragment implements AdapterView.OnItemClickListener {
 
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState){
-        super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.Plantes, android.R.layout.simple_list_item_1);
-        setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
-    }
-
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long 1){
-
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
-           Intent newIntent = new Intent (getActivity().getApplicationContext(), Main2Activity.class);
-           newIntent.setAction(Intent.ACTION_SEND);
-           newIntent.setType("text/plain");
-           newIntent.putExtra(Intent.EXTRA_TEXT,adapterView.getItemIdAtPosition(i).toString());
-           startActivity(newIntent);
-        }else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            Toast.makeText(getActivity(), text: "Item:"+ adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
-
-            Bundle bundle =new Bundle();
-            bundle.putString("KEY", adapterView.getItemIdAtPosition(i).toString());
-            FragmentViewer frag = new FragmentViewer();
-            frag.setArguments(bundle);
-
-            FragmentManager fragmentManager= getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-            fragmentTransaction.replace(R.id.viewer, frag);
-            fragmentTransaction.commit();
-
-        }
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+       View view = inflater.inflate(R.layout.list_fragment, container, )
     }
 }
---
+++ app/src/main/java/com/example/carme/carmenfragments/list_fragment.java
@@ -0,0 +1,13 @@
+package com.example.carme.carmenfragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list_fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_fragment);
+    }
+}
